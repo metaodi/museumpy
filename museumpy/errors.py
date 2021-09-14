9ada63cbe2fce18519acec6af2cd7e50a9643d9d
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2021-09-14 17:20:31 +0200

    Add new error superclass

--- museumpy/errors.py
+++ museumpy/errors.py
@@ -1,10 +1,22 @@
-class MuseumPlusError(Exception):
+class MuseumpyError(Exception):
     """
     General MuseumPlus error class to provide a superclass for all other errors
     """
 
 
-class XMLParsingError(MuseumPlusError):
+class MuseumPlusError(MuseumpyError):
+    """
+    MuseumPlus error raised when an error with the communication with MuseumPlus occurs
+    """
+
+
+class XMLParsingError(MuseumpyError):
     """
     The error raised when parsing the XML.
+    """
+
+class NoMoreRecordsError(MuseumpyError):
+    """
+    This error is raised if all records have been loaded (or no records are
+    present)
     """
